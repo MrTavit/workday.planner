bd425a7ff79454a6559a259fab36752b84f77103
Alex Camacho <alsbrain@optonline.net>
2020-10-27 14:31:15 -0700

    added color coding for hours

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ $(document).ready(function () {
     // Placeholder for Luxon DateTime function
     var dateTime = luxon.DateTime
 
-    // Adds current day to header
     dayHeader()
-
+    checkTime()
+    
+    // Adds current day to header
     function dayHeader() {
         let currentDay = document.querySelector('#currentDay')
         currentDay.innerHTML = dateTime.local().toLocaleString()
     }
+    
+    // Compares timeblock to current time
+    function checkTime() {
+        let currentHour = luxon.DateTime.local().hour
+        // Time blocks are color coded based on past, present, future
+        
+        $(".goal-box").each( function(){
+            var checkHour = parseInt($(this).attr('id'))
+            console.log(this)
+            console.log(checkHour)
+            console.log(currentHour)
+            if (checkHour < currentHour) {
+                $(this).removeClass(['present', 'future']).addClass('past')
+            }
+            else if (checkHour === currentHour) {
+                $(this).removeClass(['past', 'future']).addClass('present')
+            }
+            else {
+                $(this).removeClass(['past', 'present']).addClass('future')
+            }
+        })
+    }
 
- 
 
-
-    // Time blocks are color coded based on past, present, future
     // When clicked, an event can be typed
     // Save button can be clicked to save the event on that time block
     // When save is clicked, the text is saved to local storage
