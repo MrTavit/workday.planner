714fde95a246d270d35baf24acc88c35ea8226d7
Alex Camacho <alsbrain@optonline.net>
2020-10-27 12:22:43 -0700

    added timeblocks

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-  // Adds current day to header
-  var currentDay = document.querySelector('#currentDay')
-  currentDay.innerHTML =  luxon.DateTime.local().toLocaleString()
-  
-  // Display blocks for standard business hours
-  // 1 per hour
-  // Time blocks are color coded based on past, present, future
-  // When clicked, an event can be typed
-  // Save button can be clicked to save the event on that time block
-  // When save is clicked, the text is saved to local storage
-  // When the page is refreshed the local storage persists
-  
-  
-  
-  
+$(document).ready(function () {
+    // Placeholder for Luxon DateTime function
+    var dateTime = luxon.DateTime
+
+    // Adds current day to header
+    dayHeader()
+
+    function dayHeader() {
+        let currentDay = document.querySelector('#currentDay')
+        currentDay.innerHTML = dateTime.local().toLocaleString()
+    }
+
+ 
+
+
+    // Display blocks for standard business hours
+    // 1 per hour
+    // Time blocks are color coded based on past, present, future
+    // When clicked, an event can be typed
+    // Save button can be clicked to save the event on that time block
+    // When save is clicked, the text is saved to local storage
+    // When the page is refreshed the local storage persists
+})
+
+
